2d33b453a721b4ddc358dc920996b66567891108
lmhtvn <46315361+lmhtvn@users.noreply.github.com>
2026-03-30 19:40:55 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -18,7 +18,7 @@ export default {
 
     const incomingUrl = new URL(request.url);
     const path = incomingUrl.pathname.replace(/^\/+/, "");
-    const targetUrl = new URL(`https://mxhvn.github.io/vn/${path}${incomingUrl.search}`);
+    const targetUrl = new URL(`https://lmhtvn.github.io/thl/${path}${incomingUrl.search}`);
 
     const headers = new Headers();
     for (const [k, v] of request.headers.entries()) {
